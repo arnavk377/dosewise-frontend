8021e324641b687398d0410c51ef6d7a9e60200a
arnavk377 <arnavk1321@gmail.com>
2024-01-27 10:53:45 -0800

    changing to support multiple links

---
+++ src/assets/signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './navbar.jsx'
+
+function SignUp() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Sign Up</h1>
+        </div>
+    )
+}
+
+export default SignUp
