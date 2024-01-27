f2ecc56a24ef2aad00f995357449fc19b10afcbe
arnavk377 <arnavk1321@gmail.com>
2024-01-27 15:40:17 -0800

    work

--- src/assets/prescriptiontable.jsx
+++ src/assets/prescriptiontable.jsx
@@ -1,60 +1,64 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from './navbar.jsx';
 
 function PrescriptionTable() {
-    const handleLoad = (event) => {
-        event.preventDefault();
-        // get drug name from API request
-        const table = {data}
-        console.log(table.data);
-        // get drug info from API request
-        // display drug info
-        return;
-      }
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/get_medicines_for_user?username=jdoe')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(res => {
+                setData(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []); // <-- Fixed the misplaced comma here
+
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+
     return (
         <>
             <Navbar />
             <div>
                 <table>
-                    <tr>
-                    <th>Name</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Dosage Frequency Unit</th>
-                    <th>Dosage Frequency</th>
-                    <th>Dosage Number</th>
-                    <th></th>
-                    <th></th>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Dosage Frequency Unit</th>
+                            <th>Dosage Frequency</th>
+                            <th>Dosage Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data &&
+                            data.map(data => (
+                                <tr key={data[0]}>
+                                    <td>{data[0]}</td>
+                                    <td>{data[1]}</td>
+                                    <td>{data[2]}</td>
+                                    <td>{data[3]}</td>
+                                    <td>{data[4]}</td>
+                                    <td>
+                                        {data[7]} every {data[6]}
+                                    </td>
+                                    <td>{data[8]}</td>
+                                </tr>
+                            ))}
+                    </tbody>
                 </table>
             </div>
         </>
-    )
+    );
 }
 
-function getPrescriptionInfo() {
-    // API request to get drug info
-    const username = 'jdoe';
-    const url = 'http://localhost:8080/api/v1/get_medicines_for_user?username=' + username; //http://localhost:8080/api/v1/get_medicines_for_user?username=jdoe
-    // no-cors mode to allow cross-origin requests
-    const options = {
-      method: 'GET'
-    };
-    // fetch data and return the name of the drug
-    fetch(url, options)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response unsuccessful.');
-        }
-        return response;
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return;
-      })
-      .catch(err => {
-        alert(err.message);
-      });
-  }
-
-export default PrescriptionTable
+export default PrescriptionTable;
