a57b6b8cd5c16414c8a719e487c2d4dff0a2e210
arnavk377 <arnavk1321@gmail.com>
2024-01-27 15:57:20 -0800

    table

--- src/assets/prescriptiontable.jsx
+++ src/assets/prescriptiontable.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './navbar.jsx';
 
 function PrescriptionTable() {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null);
     useEffect(() => {
-        fetch('http://localhost:8080/api/v1/get_medicines_for_user?username=jdoe')
+        var url = 'http://localhost:8080/api/v1/get_medicines_for_user?username=' + localStorage.getItem('username');
+        // url += '&hash=' + localStorage.getItem('hash');
+        const options = {
+            method: 'GET'
+        }
+        fetch(url, options)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -12,17 +17,15 @@ function PrescriptionTable() {
                 return response.json();
             })
             .then(res => {
-                setData(res.data);
+                // console.log(res)
+                setData(res);
+                console.log(res);
             })
             .catch(err => {
                 console.log(err);
             });
-    }, []); // <-- Fixed the misplaced comma here
+    }, []);
 
-
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
 
     return (
         <>
@@ -34,24 +37,23 @@ function PrescriptionTable() {
                             <th>Name</th>
                             <th>Start Date</th>
                             <th>End Date</th>
-                            <th>Dosage Frequency Unit</th>
+                            <th>Frequency</th>
                             <th>Dosage Frequency</th>
-                            <th>Dosage Number</th>
+                            <th>Doses Remaining</th>
                         </tr>
                     </thead>
                     <tbody>
                         {data &&
                             data.map(data => (
                                 <tr key={data[0]}>
-                                    <td>{data[0]}</td>
                                     <td>{data[1]}</td>
-                                    <td>{data[2]}</td>
                                     <td>{data[3]}</td>
                                     <td>{data[4]}</td>
+                                    <td>{data[6]} every {data[5]}</td>
+                                    <td>{data[7]}</td>
                                     <td>
-                                        {data[7]} every {data[6]}
+                                        {data[7]}
                                     </td>
-                                    <td>{data[8]}</td>
                                 </tr>
                             ))}
                     </tbody>
