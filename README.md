03b9bad660d2f7d55bfc58d87004f3f99109d2fd
arnavk377 <arnavk1321@gmail.com>
2024-01-26 21:14:03 -0800

    work with danny

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# Runtime Commands
+
+```
+cd dosewise-frontend (or repository name)
+npm install
+npm run dev (for local host server)
+```
+
+# Backend Repository
+
+Link to [Backend](https://github.com/Akshay-Rohatgi/dosewise-backend)
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
@@ -6,11 +18,3 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-# Runtime Commands
-
-```
-cd dosewise-frontend (or repository name)
-npm install
-npm run dev (for local host server)
-```
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
 import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [data, setData] = useState(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // get drug name from API request
+    const drugName = {data}
+    console.log(drugName.data);
+    // get drug info from API request
+    const drugInfo = getDrugInfo(drugName.data);
+    // display drug info
+    return (
+      <>
+      <h1>MATCHED DRUG: {drugInfo}</h1>
+      </>
+    )
+  }
 
   return (
     <>
-      <h1>Welcome to Dosewise!</h1>
-      <form>
-        <label htmlFor='Name'></label>
-        <button></button>
+      <h1>Welcome to DoseWise!</h1>
+      <form className='input-form' onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='data'>Drug Name</label>
+          <input type='text' id='data' 
+          value={data || ""}
+          onChange={(e) => setData(e.target.value)}
+          />
+        </div>
+        <input type="submit"/>
       </form>
     </>
   )
 }
 
-export default App
+function getDrugInfo(drugName) {
+  // API request to get drug info
+  const url = 'http://localhost:8080/api/v1/lookup?name=' + drugName; //http://localhost:8080/api/v1/lookup?name=
+  // no-cors mode to allow cross-origin requests
+  const options = {
+    method: 'GET'
+  };
+  // fetch data and return the name of the drug
+  fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return (
+        <>
+        <h1>MATCHED DRUG: {data}</h1>
+        </>
+      )
+    })
+    .catch(err => console.log(err));
+}
+
